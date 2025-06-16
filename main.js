2e5e66b1ba414af2e7b465cb8c723e2d330e549f
dannatza <danilovukovic@tehnickacacak.edu.rs>
2025-06-16 12:10:40 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// Animacija pri skrolovanju
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+}, { threshold: 0.2 });
+
+document.querySelectorAll(".animate-on-scroll").forEach((el) => observer.observe(el));
